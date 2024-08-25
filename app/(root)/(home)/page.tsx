086fdf1689a12dcf4f2de28d82468f10fda2c357
najmulhouda <najmulhouda6@gmail.com>
2024-08-26 01:05:02 +0600

    footer and home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,15 @@
 const page = () => {
-  return <div>home</div>;
+  return (
+    <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
+      <section className="nav-padding w-full">
+        <div className="flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center">
+          <h1 className="sm:heading1 heading2 mb-6 text-center text-white">
+            BD Pro
+          </h1>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default page;
